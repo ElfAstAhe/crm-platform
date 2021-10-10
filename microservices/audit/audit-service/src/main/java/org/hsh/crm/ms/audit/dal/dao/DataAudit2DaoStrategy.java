c1c70b6dd3ddb962f8b9281abfa0a9bcbfbf7334
ElfAstAhe <elf.ast.ahe@gmail.com>
2021-10-11 01:19:42 +0300

    intermediate commit

--- microservices/audit/audit-service/src/main/java/org/hsh/crm/ms/audit/dal/dao/DataAudit2DaoStrategy.java
+++ microservices/audit/audit-service/src/main/java/org/hsh/crm/ms/audit/dal/dao/DataAudit2DaoStrategy.java
@@ -8,6 +8,7 @@ import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 import javax.transaction.Transactional;
+import java.time.OffsetDateTime;
 
 @RequestScoped
 @Named
@@ -21,11 +22,6 @@ public class DataAudit2DaoStrategy extends BaseAuditDaoStrategy<BaseDataAudit, D
     }
 
     @Override
-    protected CrudDao<DataAudit2, Long> getDao() {
-        return null;
-    }
-
-    @Override
     public AuditDaoStrategyKeyEnum getStrategyKey() {
         return AuditDaoStrategyKeyEnum.SECOND;
     }
@@ -33,5 +29,15 @@ public class DataAudit2DaoStrategy extends BaseAuditDaoStrategy<BaseDataAudit, D
     @Override
     public Class<? extends BaseDataAudit> getStrategyEntityClass() {
         return getEntityClass();
+    }
+
+    @Override
+    public boolean isEarlyExists(OffsetDateTime markerDate) {
+        return dao.getEarlyCount(markerDate) > 0L;
+    }
+
+    @Override
+    protected CrudDao<DataAudit2, Long> getDao() {
+        return dao;
     }
 }
