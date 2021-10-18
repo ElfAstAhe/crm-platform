8ffa13a42ccd2135c4b2632ded743411e8f6da48
ElfAstAhe <elf.ast.ahe@gmail.com>
2021-10-18 21:51:18 +0300

    intermediate commit

--- microservices/audit/audit-service/src/main/java/org/hsh/crm/ms/audit/dal/services/HistoryTailCutterTimer.java
+++ microservices/audit/audit-service/src/main/java/org/hsh/crm/ms/audit/dal/services/HistoryTailCutterTimer.java
@@ -1,7 +1,6 @@
 package org.hsh.crm.ms.audit.dal.services;
 
 import org.hsh.crm.ms.audit.bll.repository.AuditSettingsRepository;
-import org.hsh.crm.ms.audit.bll.settings.AuditSettingsEnum;
 import org.hsh.ms.common.bll.timer.BasePeriodicTimer;
 
 import javax.annotation.Resource;
