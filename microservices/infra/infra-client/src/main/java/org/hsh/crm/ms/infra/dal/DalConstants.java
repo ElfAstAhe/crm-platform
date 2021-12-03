404e93b8091bcff9359dfd783a702feadae7bcf9
Перепечко Алексей Варикович <AVPerepechko@sberbank.ru>
2021-12-03 09:57:02 +0300

    develop: add blank infra micro-service module

---
+++ microservices/infra/infra-client/src/main/java/org/hsh/crm/ms/infra/dal/DalConstants.java
@@ -0,0 +1,9 @@
+package org.hsh.crm.ms.infra.dal;
+
+public class DalConstants {
+    public static final String DATA_SOURCE = "jdbc/crm_infra";
+
+    private DalConstants() {
+        // hide
+    }
+}
